8d3880a73f3b0490b246d00b84fc221a8f6123da
George DS Brooks <george@gdsbrooks.com>
2021-07-02 16:40:49 +0100

    random cocktail working. should export to a middleware that can be called on 404 too

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -6,8 +6,17 @@ const DrinkModel = require('../models/Drink.model')
 ///Search ROUTES///
 // - random, by ID, 
 
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/random", async (req, res, next) =>  {
+  try {
+    const drinkCount = await DrinkModel.countDocuments()
+    const random = await Math.floor(Math.random() * drinkCount)
+    const result = await DrinkModel.findOne().skip(random)
+    await res.render("testgeorge.hbs", {result})
+  } 
+  catch(err) {
+    next(err)
+  }
+  
 });
 
 module.exports = router;
