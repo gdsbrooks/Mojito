ef509e9888eb4275cdeaa30c80916160f921550f
George DS Brooks <george@gdsbrooks.com>
2021-07-05 11:22:10 +0100

    search route implemented - dynamic get to test query. Query retrieves by name and ingredient, sorts by name

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -19,4 +19,18 @@ router.get("/random", async (req, res, next) =>  {
   
 });
 
+router.get("/search/:searchTerm", (req, res, next) => {
+  const searchTerm = req.params.searchTerm
+  
+  DrinkModel.find({$or: [{"ingredients.name": { $regex: searchTerm, $options: "i" }} , {name: { $regex: searchTerm, $options: "i"  }}]})
+  .sort({name: 1})
+  .then((result) => {
+    res.render("search.hbs", {searchTerm, result})
+  
+  })
+  .catch((err) => {
+    next(err)
+  }); 
+})
+
 module.exports = router;
