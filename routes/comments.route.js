d1abf6ee080b60fb855d4254587d1c6acdbfe97a
emmyuxui <emmyuxui@gmail.com>
2021-07-06 18:17:19 +0200

    updated copy to about us

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -5,17 +5,17 @@ const DrinkModel = require('../models/Drink.model')
 
 //CREATE NOTE
 // <form action="/post-create" method="POST">
-router.post('/fav-create', (req, res, next) => {
-    const { title, content, author } = req.body;
+router.post('/comment-create', (req, res, next) => {
+    const { comment, nickname } = req.body;
     // 'author' represents the ID of the user document
    
-    Post.create({ title, content, author })
+    Post.create({ comment, nickname})
       .then(dbPost => {
         // when the new post is created, the user needs to be found and its posts updated with the
         // ID of newly created post
-        return User.findByIdAndUpdate(author, { $push: { posts: dbPost._id } });
+        return User.findByIdAndUpdate(nickname, { $push: { posts: dbPost._id } });
       })
-      .then(() => res.redirect('/posts')) // if everything is fine, redirect to list of posts
+      .then(() => res.redirect('/comments')) // if everything is fine, redirect to list of posts
       .catch(err => {
         console.log(`Err while creating the post in the DB: ${err}`);
         next(err);
@@ -31,9 +31,9 @@ router.post('/fav-create', (req, res, next) => {
 //  EXAMPLES HERE 
 https://my.ironhack.com/lms/courses/course-v1:IRONHACK+WDFT52+202105_RMT/modules/ironhack-course-chapter_4/units/ironhack-course-chapter_5-sequential-vertical_1
 // ****************************************************************************************
-router.get('/posts', (req, res, next) => {
+router.get('/comments', (req, res, next) => {
     Post.find()
-      .populate('author') // --> we are saying: give me whole user object with this ID (author represents an ID in our case)
+      .populate('anickname') // --> we are saying: give me whole user object with this ID (author represents an ID in our case)
       .then(dbPosts => {
         // console.log("Posts from the DB: ", dbPosts);
         res.render('posts/list', { posts: dbPosts });
